abd0c178c641ebdc9b7024de028a14023e19a2ea
Amber Taranto <ambzzz@gmail.com>
2020-08-02 19:28:29 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  let newState = { ...state };
+  switch (action.type) {
+    case "ADD_FRIEND":
+      newState.friends.push(action.friend);
+      return newState;
+    //   return { friends: state.friends.push(action.friend) };
+    case "REMOVE_FRIEND":
+      newState.friends = newState.friends.filter(
+        (friend) => action.id != friend.id
+      );
+      return newState;
+    default:
+      return state;
+  }
 }
